eb18a4532583e0aa8e7ad346f32d8468e1a59586
Valentina Santa Muñoz <valentinas792@gmail.com>
2023-06-06 21:13:10 -0500

    Fix: Persistence port
    Persistence port for Employee

--- src/test/java/com/pragma/powerup/squaremicroservice/domain/usecase/RestaurantUseCaseTest.java
+++ src/test/java/com/pragma/powerup/squaremicroservice/domain/usecase/RestaurantUseCaseTest.java
@@ -3,6 +3,7 @@ package com.pragma.powerup.squaremicroservice.domain.usecase;
 import com.pragma.powerup.squaremicroservice.adapters.driven.jpa.mysql.entity.RestaurantEntity;
 import com.pragma.powerup.squaremicroservice.adapters.driven.jpa.mysql.exceptions.RestaurantNotFoundException;
 import com.pragma.powerup.squaremicroservice.adapters.driven.jpa.mysql.repositories.IEmployeeRepository;
+import com.pragma.powerup.squaremicroservice.adapters.driving.http.dto.request.EmployeeRequestDto;
 import com.pragma.powerup.squaremicroservice.domain.exceptions.UserNotBeAOwnerException;
 import com.pragma.powerup.squaremicroservice.adapters.driven.jpa.mysql.repositories.IRestaurantRepository;
 import com.pragma.powerup.squaremicroservice.adapters.driving.http.adapter.EmployeeHttpAdapter;
@@ -13,6 +14,7 @@ import com.pragma.powerup.squaremicroservice.domain.exceptions.PageNotFoundExcep
 import com.pragma.powerup.squaremicroservice.domain.model.Employee;
 import com.pragma.powerup.squaremicroservice.domain.model.Restaurant;
 import com.pragma.powerup.squaremicroservice.domain.model.User;
+import com.pragma.powerup.squaremicroservice.domain.spi.IEmployeeHttpAdapterPersistencePort;
 import com.pragma.powerup.squaremicroservice.domain.spi.IEmployeePersistencePort;
 import com.pragma.powerup.squaremicroservice.domain.spi.IRestaurantPersistencePort;
 import org.junit.jupiter.api.Assertions;
@@ -21,14 +23,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
-import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -51,15 +50,18 @@ class RestaurantUseCaseTest {
     @Mock
     private IEmployeePersistencePort employeePersistencePort;
 
+    @Mock
+    private IEmployeeHttpAdapterPersistencePort employeeHttpAdapterPersistencePort;
 
+    @Mock
+    private RestTemplate restTemplate;
 
-    @InjectMocks
     private RestaurantUseCase restaurantUseCase;
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        restaurantUseCase = new RestaurantUseCase(restaurantPersistencePort,employeePersistencePort, restaurantRepository);
+        restaurantUseCase = new RestaurantUseCase(restaurantPersistencePort,employeePersistencePort, restaurantRepository, employeeHttpAdapterPersistencePort);
+        restTemplate = new RestTemplate();
     }
 
 
@@ -74,6 +76,7 @@ class RestaurantUseCaseTest {
                 new Restaurant(15L, "Mi Restaurante 3", "carrera 13 #12-12","456789","image.jpg",1L, "123456")
         );
         when(restaurantPersistencePort.getAllRestaurants(page, pageSize)).thenReturn(restaurants);
+
 
         // Act
         List<Restaurant> result = restaurantPersistencePort.getAllRestaurants(page, pageSize);
@@ -132,38 +135,40 @@ class RestaurantUseCaseTest {
     void testAddEmployee() {
         // Arrange
         Long idRestaurant = 123L;
-        Long idEmployee = 456L;
-        Employee employee = new Employee(6L, 456L, "12345678", 123L);
-
+        Long idEmployee = 52L;
         Long idOwnerToken = 123456789L;
 
         Interceptor.setIdUser(idOwnerToken);
 
+        EmployeeRequestDto employeeRequestDto = new EmployeeRequestDto(52L, 123L);
+
         User user = new User();
+        user.setId(idEmployee);
         user.setDniNumber("12345678");
         user.setIdRole(Constants.EMPLOYEE_ROLE_ID);
-
-        Restaurant restaurant = new Restaurant(123L, "Alitas BBQ", "carrera 13 #12-12",
-                "456789",
-                "imagen.jpg",
-                123456789L, "123456");
 
         RestaurantEntity restaurantEntity = new RestaurantEntity(123L, "Alitas BBQ", "carrera 13 #12-12",
                 "456789",
                 "imagen.jpg",
-                123456789L, "123456");
+                idOwnerToken, "123456");
 
-        when(employeeHttpAdapter.getEmployee(idEmployee)).thenReturn(user);
+        Employee employee = new Employee(6L, 52L,"123456789",123L);
+
+
         when(restaurantRepository.findById(restaurantEntity.getId())).thenReturn(Optional.of(restaurantEntity));
-        employeePersistencePort.addEmployee(employee);
-
+        when(employeeHttpAdapter.getEmployee(employeeRequestDto.getIdEmployee())).thenReturn(user);
+        doNothing().when(employeePersistencePort).addEmployee(employee);
 
         // Act
         restaurantUseCase.addEmployee(employee);
 
         // Assert
+        Assertions.assertDoesNotThrow(() -> {
+            restaurantUseCase.addEmployee(employee);
+        });
         verify(employeeHttpAdapter, times(1)).getEmployee(idEmployee);
         verify(employeePersistencePort, times(2)).addEmployee(employee);
+
 
     }
 
